87f2ea2a9eb1469ee217748064125ebada241ada
Sanjay Siddharam Hulbute <64067162+SanjayHulbute@users.noreply.github.com>
2021-04-08 22:29:04 +0530

    Delete backend.tf

--- TERRAFORM_CODE1/CTL-DevOpsBatch-Feb2021-main/Terraform/workspace-demo/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-dynamodb-lock-tf"
-    key            = "workspace-demo"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform_state"
-  }
-}
