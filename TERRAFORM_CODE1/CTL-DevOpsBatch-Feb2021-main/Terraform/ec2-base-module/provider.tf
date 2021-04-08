68975e38a7b65d19c0222c420cd86288b480398d
Sanjay Siddharam Hulbute <64067162+SanjayHulbute@users.noreply.github.com>
2021-04-08 22:22:40 +0530

    Delete provider.tf

--- TERRAFORM_CODE1/CTL-DevOpsBatch-Feb2021-main/Terraform/ec2-base-module/provider.tf
+++
@@ -1,7 +0,0 @@
-
-provider "aws" {
-  region = "ap-south-1"
-  #shared_credentials_file = "/Users/DELL/.aws/credentials"
-  access_key = "AKIAXKERQJZFL5P352GL"
-  secret_key = "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"
-}
